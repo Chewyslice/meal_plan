471b083e6ea7f1809f5bc063fde2e545de2d0c1f
Chewyslice <chewyslice@gmail.com>
2017-07-05 20:52:17 -0500

    finished editing create_recipes.rb
    finished editing create_recipes.rb

--- db/migrate/20170706011208_create_recipes.rb
+++ db/migrate/20170706011208_create_recipes.rb
@@ -4,8 +4,10 @@ class CreateRecipes < ActiveRecord::Migration[5.1]
       t.string :name. null: false
       t.text :description, null: false
       t.belongs_to :user. null: false, foreign_key: true
+      
       t.timestamps null: false
-
     end
+
+    add_index :recipes, [:user_id, :name]
   end
 end

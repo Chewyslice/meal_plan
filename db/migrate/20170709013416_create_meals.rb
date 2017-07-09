bea6671959f6da4b80da94d486abcfe35dc5b47a
Chewyslice <chewyslice@gmail.com>
2017-07-08 20:41:09 -0500

    changes reference to references dur

--- db/migrate/20170709013416_create_meals.rb
+++ db/migrate/20170709013416_create_meals.rb
@@ -2,8 +2,8 @@ class CreateMeals < ActiveRecord::Migration[5.1]
   def change
     create_table :meals do |t|
       t.date :date, null: false
-      t.reference :meal_plan, nul: false, foreign_key: true
-      t.reference :recipe, nul: false, foreign_key: true
+      t.references :meal_plan, nul: false, foreign_key: true
+      t.references :recipe, nul: false, foreign_key: true
       t.timestamps null: false
 
     end

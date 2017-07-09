116f731187fb6f46a012cc43cfdfa268f5d0fa03
Chewyslice <chewyslice@gmail.com>
2017-07-08 20:38:51 -0500

    edited create_meals migration

--- db/migrate/20170709013416_create_meals.rb
+++ db/migrate/20170709013416_create_meals.rb
@@ -1,6 +1,11 @@
 class CreateMeals < ActiveRecord::Migration[5.1]
   def change
     create_table :meals do |t|
+      t.date :date, null: false
+      t.reference :meal_plan, nul: false, foreign_key: true
+      t.reference :recipe, nul: false, foreign_key: true
+      t.timestamps null: false
+
     end
   end
 end
